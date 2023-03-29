4879ee6d91758c3e6c82223376b40b12437d5676
FirdausyTama <atama.firdausy@gmail.com>
2023-03-29 14:10:38 +0700

    untuk memasukan nama kandidat, nilai matematika, nilai bahasa inggris

--- ExerciseProsedural_021/ExerciseProsedural_021.cpp
+++ ExerciseProsedural_021/ExerciseProsedural_021.cpp
@@ -1,3 +1,17 @@
 #include <iostream>
 using namespace std;
 
+int main() {
+	int nilai_matematika[20];
+	int nilai_bahasa_inggris[20];
+	string nama_kandidat[20];
+	for (int i = 0; i < 20; i++) {
+		cout << "======================" << endl;
+		cout << "Masukan nama kandidat: ";
+		cin >> nama_kandidat[i];
+		cout << "Masukan nilai matematika: ";
+		cin >> nilai_matematika[i];
+		cout << "Masukan nilai bahasa inggris: ";
+		cin >> nilai_bahasa_inggris[i];
+	}
+}
