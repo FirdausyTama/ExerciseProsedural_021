afcd8922aff869cf93073ea6abfa93f20c99bf7a
FirdausyTama <atama.firdausy@gmail.com>
2023-03-29 14:21:12 +0700

    penentuan status di terima atau tidak di terima

--- ExerciseProsedural_021/ExerciseProsedural_021.cpp
+++ ExerciseProsedural_021/ExerciseProsedural_021.cpp
@@ -14,4 +14,18 @@ int main() {
 		cout << "Masukan nilai bahasa inggris: ";
 		cin >> nilai_bahasa_inggris[i];
 	}
+
+	int jumlah_diterima = 0;
+	int jumlah_tidak_diterima = 0;
+	for (int i = 0; i < 20; i++) {
+		float rata_rata = (nilai_matematika[i] + nilai_bahasa_inggris[i]) / 2.0;
+		if (rata_rata >= 70 || nilai_matematika[i] > 80) {
+			cout << nama_kandidat[i] << "\nDiterima" << endl;
+			jumlah_diterima++;
+		}
+		else {
+			cout << nama_kandidat[i] << "\nTidak diterima" << endl;
+			jumlah_tidak_diterima++;
+		}
+	}
 }
