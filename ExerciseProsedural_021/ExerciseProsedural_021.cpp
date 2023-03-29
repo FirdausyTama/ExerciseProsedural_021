747a5b8adbc292b28cb067ba7545610920fb60fc
FirdausyTama <atama.firdausy@gmail.com>
2023-03-29 14:29:33 +0700

    ada perubahan dari tidak diterima menjadi di tolak

--- ExerciseProsedural_021/ExerciseProsedural_021.cpp
+++ ExerciseProsedural_021/ExerciseProsedural_021.cpp
@@ -6,7 +6,7 @@ int main() {
 	int nilai_bahasa_inggris[20];
 	string nama_kandidat[20];
 	for (int i = 0; i < 20; i++) {
-		cout << "======================" << endl;
+		cout << "================================" << endl;
 		cout << "Masukan nama kandidat: ";
 		cin >> nama_kandidat[i];
 		cout << "Masukan nilai matematika: ";
@@ -16,7 +16,7 @@ int main() {
 	}
 
 	int jumlah_diterima = 0;
-	int jumlah_tidak_diterima = 0;
+	int jumlah_ditolak = 0;
 	for (int i = 0; i < 20; i++) {
 		float rata_rata = (nilai_matematika[i] + nilai_bahasa_inggris[i]) / 2.0;
 		if (rata_rata >= 70 || nilai_matematika[i] > 80) {
@@ -24,13 +24,13 @@ int main() {
 			jumlah_diterima++;
 		}
 		else {
-			cout << nama_kandidat[i] << "\nTidak diterima" << endl;
-			jumlah_tidak_diterima++;
+			cout << nama_kandidat[i] << "\nDitolak" << endl;
+			jumlah_ditolak++;
 		}
 	}
 
 	cout << "jumlah kandidat yang di terima: " << jumlah_diterima << endl;
-	cout << "jumlah kandidat yang tidak di terima: " << jumlah_tidak_diterima << endl;
+	cout << "jumlah kandidat yang ditolak: " << jumlah_ditolak << endl;
 
 	return 0;
 }
